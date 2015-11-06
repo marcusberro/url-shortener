b01141c4aa0a68119e27e4b70980f30696cd14ba
Marcus Berro <marcusberro@gmail.com>
2015-11-06 00:06:45 -0200

    List shorten urls service and saving some header info when redirecting

--- src/main/java/com/mberro/urlshortener/application/ShortenerApplicationService.java
+++ src/main/java/com/mberro/urlshortener/application/ShortenerApplicationService.java
@@ -3,12 +3,14 @@ package com.mberro.urlshortener.application;
 import com.mberro.urlshortener.application.representation.ShortenResponse;
 import com.mberro.urlshortener.domain.ShortenUrl;
 import com.mberro.urlshortener.domain.ShortenerService;
+import com.mberro.urlshortener.domain.repository.ShortenerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,6 +25,9 @@ public class ShortenerApplicationService {
     @Autowired
     private ShortenerService shortenerService;
 
+    @Autowired
+    private ShortenerRepository shortenerRepository;
+
     public ShortenResponse shorten(String url) {
         ShortenUrl shortenUrl = shortenerService.shorten(url);
         return new ShortenResponse(shortenUrl.getId(), shortenUrl.getCode(), shortenUrl.getCreationDate(), true, null);
@@ -30,5 +35,9 @@ public class ShortenerApplicationService {
 
     public String getRedirectionUrl(String code, Map<String, String> requestInfo) {
         return shortenerService.getRedirectionUrl(code, requestInfo);
+    }
+
+    public List<ShortenUrl> listShortenUrls(){
+        return shortenerRepository.findAll();
     }
 }
--- src/main/java/com/mberro/urlshortener/presentation/ShortenerController.java
+++ src/main/java/com/mberro/urlshortener/presentation/ShortenerController.java
@@ -2,6 +2,7 @@ package com.mberro.urlshortener.presentation;
 
 import com.mberro.urlshortener.application.ShortenerApplicationService;
 import com.mberro.urlshortener.application.representation.ShortenResponse;
+import com.mberro.urlshortener.domain.ShortenUrl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Marcus Berro on 31/10/15.
@@ -23,22 +26,33 @@ public class ShortenerController {
     @Autowired
     private ShortenerApplicationService shortenerApplicationService;
 
-    @RequestMapping(path = "/{code}", method = RequestMethod.GET)
-    public void redirect(@PathVariable String code, HttpServletResponse response) throws IOException {
-        // TODO colocar header info
-        response.sendRedirect(shortenerApplicationService.getRedirectionUrl(code, new HashMap<>()));
-    }
-
     @RequestMapping(path = "/", method = RequestMethod.POST)
     @ResponseBody
     public ShortenResponse shorten(@RequestBody String url, HttpServletRequest request) throws IOException {
-        // TODO colocar header info??
         return shortenerApplicationService.shorten(url);
     }
 
+    @RequestMapping(path = "/shorten-urls", method = RequestMethod.GET)
+    public List<ShortenUrl> listShortenUrls() {
+        return shortenerApplicationService.listShortenUrls();
+    }
+
+    @RequestMapping(path = "/{code}", method = RequestMethod.GET)
+    public void redirect(@PathVariable String code, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.sendRedirect(shortenerApplicationService.getRedirectionUrl(code, getSourceInfoMap(request)));
+    }
+
+    // TODO build web app to show and create shorten URLs
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String helloWorld() {
         return "Home";
     }
 
+    private Map<String, String> getSourceInfoMap(HttpServletRequest request) {
+        Map<String, String> sourceInfo = new HashMap<>();
+        sourceInfo.put("userAgent", request.getHeader("User-Agent"));
+        sourceInfo.put("sourceHost",request.getHeader("Host"));
+        sourceInfo.put("sourceRequestDate", request.getHeader("Date"));
+        return sourceInfo;
+    }
 }
